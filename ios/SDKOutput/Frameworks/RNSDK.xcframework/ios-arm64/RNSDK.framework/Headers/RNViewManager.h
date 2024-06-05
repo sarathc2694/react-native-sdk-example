ed2d844c14032e839cbfd206d48fb85002e616e7
sarath <sarathc2694@gmail.com>
2024-06-05 15:11:29 +0530

    Added test framework and resources

---
+++ ios/SDKOutput/Frameworks/RNSDK.xcframework/ios-arm64/RNSDK.framework/Headers/RNViewManager.h
@@ -0,0 +1,17 @@
+//
+//  RNViewManager.h
+//  RNSDK
+//
+//  Created by Sarath Chenthamarai on 03/06/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNViewManager : NSObject
++ (UIView *)getReactNativeView;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/SDKOutput/Frameworks/RNSDK.xcframework/ios-arm64_x86_64-simulator/RNSDK.framework/Headers/RNSDK.h
@@ -0,0 +1,19 @@
+//
+//  RNSDK.h
+//  RNSDK
+//
+//  Created by Sarath Chenthamarai on 03/06/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <RNSDK/RNViewManager.h>
+
+//! Project version number for RNSDK.
+FOUNDATION_EXPORT double RNSDKVersionNumber;
+
+//! Project version string for RNSDK.
+FOUNDATION_EXPORT const unsigned char RNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNSDK/PublicHeader.h>
+
+
